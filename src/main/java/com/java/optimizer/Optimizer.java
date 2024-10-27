5a9d0e35b394c0fe65f4fe649d45f84ade47cb2d
m0t9 <matveykorinenko@gmail.com>
2024-10-27 22:25:15 +0300

    remove unused variables

--- src/main/java/com/java/optimizer/Optimizer.java
+++ src/main/java/com/java/optimizer/Optimizer.java
@@ -1,7 +1,8 @@
 package com.java.optimizer;
 
 import com.java.lexer.Lexer;
-import com.java.optimizer.visitor.UnreachableCodeVisitor;
+import com.java.optimizer.visitor.UnreachableCodeOmitter;
+import com.java.optimizer.visitor.UnusedVariablesOptimizer;
 import com.java.parser.Parser;
 import com.java.parser.ast.visitor.PrintVisitor;
 import java.io.IOException;
@@ -9,31 +10,17 @@ import java.io.IOException;
 public class Optimizer {
     public static void main(String[] args) throws IOException {
         var program = """
-                var f := func(n) is
-                    var arr := [1, 2, 3];
-                    while i < 10 loop
-                        return 10;
-                        print i;
-                    end
-                    for i in arr loop
-                        if i = 2 then
-                            i := i + 1;
-                            return 10;
-                            i := 3;
-                        end
-                        i := 4;
-                        return 12;
-                        i := 5;
-                    end
-                    n := n + 1;
-                    return 10;
-                    return 20;
-                    print n;
-                end;
+                var arr := [1, 2, 3];
+                var x := [2, 3];
+
+                for item in arr loop
+                    var x := item;
+                    print (x * x);
+                end
                 """;
         var p = Parser.makeAST(new Lexer(program));
         p.accept(new PrintVisitor(0));
-        p.accept(new UnreachableCodeVisitor());
+        p.accept(new UnusedVariablesOptimizer());
         p.accept(new PrintVisitor(0));
     }
 }
--- src/main/java/com/java/optimizer/visitor/UnusedVariablesOptimizer.java
+++ src/main/java/com/java/optimizer/visitor/UnusedVariablesOptimizer.java
@@ -37,7 +37,13 @@ import com.java.parser.ast.node.real.UnaryOp;
 import com.java.parser.ast.node.real.WhileStatement;
 import com.java.parser.ast.visitor.ASTVisitor;
 
-public class UnreachableCodeVisitor implements ASTVisitor<Object> {
+import java.util.*;
+
+public class UnusedVariablesOptimizer implements ASTVisitor<Object> {
+    private final List<Map<String, Boolean>> scopesUsage = new LinkedList<>();
+    private final List<Map<String, DeclarationsCommaList>> scopesDeclValues = new LinkedList<>();
+    private final List<Map<String, OnlyIdentifiersDeclarationStatement>> scopesDeclEmpty = new LinkedList<>();
+
     @Override
     public Object visitAST(ASTree ast) {
         return ast.getNodes().accept(this);
@@ -45,32 +51,42 @@ public class UnreachableCodeVisitor implements ASTVisitor<Object> {
 
     @Override
     public Object visitAccessTailList(AccessTailList accessTailList) {
+        for (var t: accessTailList.getTails()) {
+            t.accept(this);
+        }
         return null;
     }
 
     @Override
     public Object visitArrayAccess(ArrayAccess arrayAccessNode) {
-        return null;
+        return arrayAccessNode.getExpression().accept(this);
     }
 
     @Override
     public Object visitArray(Array array) {
-        return null;
+        return array.getElements().accept(this);
     }
 
     @Override
     public Object visitBinaryOperation(BinaryOp node) {
+        node.getLeft().accept(this);
+        node.getRight().accept(this);
         return null;
     }
 
     @Override
     public Object visitComparisonOp(ComparisonOp comparisonOp) {
+        comparisonOp.getLeft().accept(this);
+        comparisonOp.getRight().accept(this);
         return null;
     }
 
     @Override
     public Object visitDeclarationsCommaList(DeclarationsCommaList declarationsCommaList) {
-        for (var decl : declarationsCommaList.getDeclarations()) {
+        for (var decl: declarationsCommaList.getDeclarations()) {
+            var name = decl.getIdentifier().lexeme();
+            var scope = declarationsCommaList;
+            declare(name, scope);
             decl.accept(this);
         }
         return null;
@@ -83,125 +99,147 @@ public class UnreachableCodeVisitor implements ASTVisitor<Object> {
 
     @Override
     public Object visitExpression(Expression expression) {
-        return null;
+        return expression.getBody().accept(this);
     }
 
     @Override
     public Object visitExpressionStatement(ExpressionStatement expressionStatementNode) {
-        return null;
+        return expressionStatementNode.getExpression().accept(this);
     }
 
     @Override
     public Object visitExpressionsCommaList(ExpressionsCommaList expressionsCommaList) {
-        return null;
-    }
-
-    @Override
-    public Object visitForStatement(ForStatement forStatementNode) {
-        return forStatementNode.getLoopBody().accept(this);
-    }
-
-    @Override
-    public Object visitFunctionCall(FunctionCall funCall) {
-        return null;
-    }
-
-    @Override
-    public Object visitFunctionLiteral(FunctionLiteral funLiteral) {
-        if (funLiteral.getFunBody() instanceof StatementsList) {
-            return funLiteral.getFunBody().accept(this);
+        for (var expr: expressionsCommaList.getExpressions()) {
+            expr.accept(this);
         }
         return null;
     }
 
     @Override
+    public Object visitForStatement(ForStatement forStmt) {
+        use(forStmt.getTypeIndicator().lexeme());
+        prepareScope();
+        forStmt.getLoopBody().accept(this);
+        optimizeUsagesAndLeave();
+        return null;
+    }
+
+    @Override
+    public Object visitFunctionCall(FunctionCall funCall) {
+        return funCall.getExpressions().accept(this);
+    }
+
+    @Override
+    public Object visitFunctionLiteral(FunctionLiteral funLiteral) {
+        prepareScope();
+        funLiteral.getFunBody().accept(this);
+        optimizeUsagesAndLeave();
+        return null;
+    }
+
+    @Override
     public Object visitIdentifierAssign(IdentifierAssign identifierAssignNode) {
+        use(identifierAssignNode.getIdentifier().lexeme());
+        identifierAssignNode.getExpression().accept(this);
         return null;
     }
 
     @Override
     public Object visitIdentifierWithValue(IdentifierWithValue identifierWithValue) {
-        return identifierWithValue.getValue().accept(this);
+        identifierWithValue.getValue().accept(this);
+        return null;
     }
 
     @Override
-    public Object visitIdentifiersCommaList(IdentifiersCommaList tokenListNode) {
+    public Object visitIdentifiersCommaList(IdentifiersCommaList idents) {
         return null;
     }
 
     @Override
     public Object visitIdentifiersWithValueDeclarationStatement(IdentifiersWithValueDeclarationStatement multipleDeclarationsStatement) {
-        System.out.println("DECLS STMT");
         return multipleDeclarationsStatement.getDeclarations().accept(this);
     }
 
     @Override
     public Object visitIfStatement(IfStatement ifStatementNode) {
+        ifStatementNode.getExpression().accept(this);
         ifStatementNode.getTrueBody().accept(this);
         if (ifStatementNode.getFalseBody() != null) {
-            return ifStatementNode.getFalseBody().accept(this);
+            ifStatementNode.getFalseBody().accept(this);
         }
         return null;
     }
 
     @Override
     public Object visitLogicalOp(LogicalOp logicalOp) {
+        logicalOp.getLeft().accept(this);
+        logicalOp.getRight().accept(this);
         return null;
     }
 
     @Override
     public Object visitLoopBody(LoopBody loopBodyNode) {
-        return loopBodyNode.getLoopBody().accept(this);
+        loopBodyNode.getLoopBody().accept(this);
+        return null;
     }
 
     @Override
     public Object visitOnlyIdentifiersDeclarationStatement(OnlyIdentifiersDeclarationStatement node) {
+        for (var ident: node.getIdentifiers().getTokens()) {
+            declare(ident.lexeme(), node);
+        }
         return null;
     }
 
     @Override
     public Object visitPrintStatement(PrintStatement printStatementNode) {
+        printStatementNode.getExpressions().accept(this);
         return null;
     }
 
     @Override
     public Object visitReadStatement(ReadStatement readNode) {
+        readNode.getDest().accept(this);
         return null;
     }
 
     @Override
     public Object visitReferenceAssignStatement(ReferenceAssignStatement referenceAssignStatementNode) {
+        use(referenceAssignStatementNode.getIdentifier().lexeme());
+        referenceAssignStatementNode.getExpression().accept(this);
         return null;
     }
 
     @Override
     public Object visitReferenceTail(ReferenceTail referenceTail) {
+        use(referenceTail.getIdentifier().lexeme());
+        if (referenceTail.getTail() != null) {
+            referenceTail.getTail().accept(this);
+        }
         return null;
     }
 
     @Override
     public Object visitReferenceType(ReferenceType referenceTypeNode) {
+        referenceTypeNode.getReference().accept(this);
         return null;
     }
 
     @Override
     public Object visitReturnStatement(ReturnStatement returnStatementNode) {
+        if (returnStatementNode.getExpression() != null) {
+            returnStatementNode.getExpression().accept(this);
+        }
         return null;
     }
 
     @Override
     public Object visitStatementsList(StatementsList statementsList) {
-        System.out.println("STMTS");
-        var stmts = statementsList.getStatements();
-        for (int i = 0; i < stmts.size(); i++) {
-            if (stmts.get(i) instanceof ReturnStatement) {
-                while (stmts.size() > i + 1) {
-                    stmts.removeLast();
-                }
-                break;
-            }
-            stmts.get(i).accept(this);
+        prepareScope();
+        for (var stmt: statementsList.getStatements()) {
+            stmt.accept(this);
         }
+        optimizeUsagesAndLeave();
         return null;
     }
 
@@ -212,7 +250,7 @@ public class UnreachableCodeVisitor implements ASTVisitor<Object> {
 
     @Override
     public Object visitTuple(Tuple tuple) {
-        return null;
+        return tuple.getElements().accept(this);
     }
 
     @Override
@@ -222,16 +260,72 @@ public class UnreachableCodeVisitor implements ASTVisitor<Object> {
 
     @Override
     public Object visitTupleList(TupleList tupleListNode) {
+        for (var expr: tupleListNode.getAllExpressions()) {
+            expr.accept(this);
+        }
         return null;
     }
 
     @Override
     public Object visitUnaryOp(UnaryOp unaryOpNode) {
+        unaryOpNode.getUnary().accept(this);
         return null;
     }
 
     @Override
     public Object visitWhileStatement(WhileStatement whileStatementNode) {
-        return whileStatementNode.getLoopBody().accept(this);
+        whileStatementNode.getExpression().accept(this);
+        whileStatementNode.getLoopBody().accept(this);
+        return null;
+    }
+
+    private void use(String name) {
+        for (var scope: scopesUsage.reversed()) {
+            if (scope.containsKey(name)) {
+                scope.put(name, true);
+                break;
+            }
+        }
+    }
+
+    private void declare(String name, DeclarationsCommaList parent) {
+        scopesUsage.getLast().put(name, false);
+        scopesDeclValues.getLast().put(name, parent);
+    }
+
+    private void declare(String name, OnlyIdentifiersDeclarationStatement parent) {
+        scopesUsage.getLast().put(name, false);
+        scopesDeclEmpty.getLast().put(name, parent);
+    }
+
+    private void optimizeUsagesAndLeave() {
+        var usage = scopesUsage.getLast();
+        for (var entry: usage.entrySet()) {
+            if (entry.getValue()) {
+                continue;
+            }
+            removeUnusedDeclaration(entry.getKey());
+        }
+    }
+
+    private void removeUnusedDeclaration(String name) {
+        var scopeDeclared = scopesDeclValues.getLast();
+        var scopeEmpty = scopesDeclEmpty.getLast();
+
+        var declaration = scopeDeclared.get(name);
+        if (declaration != null) {
+            declaration.getDeclarations().removeIf(id -> id.getIdentifier().lexeme().equals(name));
+        }
+
+        var emptyDecl = scopeEmpty.get(name);
+        if (emptyDecl != null) {
+            emptyDecl.getIdentifiers().getTokens().removeIf(tkn -> tkn.lexeme().equals(name));
+        }
+    }
+
+    private void prepareScope() {
+        scopesUsage.add(new HashMap<>());
+        scopesDeclValues.add(new HashMap<>());
+        scopesDeclEmpty.add(new HashMap<>());
     }
 }
